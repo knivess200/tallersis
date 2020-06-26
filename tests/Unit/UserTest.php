0fc602a473a5c136ab38e6577c4fc87ad07b82cb
unknown <knivess200@gmail.com>
2020-06-26 00:39:39 -0400

    prueba fallida

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -84,7 +84,7 @@ class UserTest extends TestCase
             'email' => 'ceso@ceso.com',
             'password' => '123456789',
         ]);
-         $response->assertSessionHasErrors('lastname');
+         $response->assertSessionHasErrors('laaaaaastname');
 
     }
 
